3e3aa0229e0c202f3ba21c2920ff430a2f3bd9a4
swaghaiboss <156926330+swaghaiboss@users.noreply.github.com>
2024-03-25 17:23:42 +0000

    basic_variables part one

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 1223344
+var accountEmail = "swaghaiboss@gamil.com"
+let accountPassword = "12345"
+accountCity = "rajapur"
+let accountState;
+
+// accountId = 3
+console.log(accountId);
+accountEmail = "mukeshswain2201@gmail.com"
+accountPassword = "13121233"
+accountCity = "digapahandi"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
