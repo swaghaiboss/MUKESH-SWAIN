d736f40bae34585436e7c1ed870636230c4ba0a0
swaghaiboss <156926330+swaghaiboss@users.noreply.github.com>
2024-03-26 12:03:54 +0000

    datatypes

---
+++ 01_basic/02_Datatypes.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// alert(2 + 3) we are using nodejs not browser
+
+
+console.log(3 + 3);  console.log("Mukesh")
+
+let name = "Mukesh"
+let age = 23
+let isLoggedIn = true
+
+//number 2 to the power 53
+// string ""
+// bigInt
+//boolean => true/false
+//null => standloan value
+//undefined
+//symbol
+
+console.log(typeof "")
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
