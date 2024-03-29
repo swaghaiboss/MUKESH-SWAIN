b37f82977bf6370d672bcca08bd84149985b220e
swaghaiboss <156926330+swaghaiboss@users.noreply.github.com>
2024-03-29 15:34:42 +0000

    conversion

---
+++ 01_basic/03_conversion.js
@@ -0,0 +1,42 @@
+let score = "33abc"
+
+// console.log(typeof (score))
+// console.log(typeof score)
+
+let valueInNumber = Number(score)
+// console.log(typeof (valueInNumber))
+// console.log(valueInNumber)
+
+//"33" => 33
+// "33abc" => NaN
+// true =>1/ false=> 0
+
+let isLoggedIn = "mukesh"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 = true / 0 = false
+// "" = false
+//"mukesh" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+// ************* operations********************
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+let str1 = "hello"
+let str2 = " Mukesh"
+
+let str3 = str1 + str2
+console.log(str3)
+
+let gameCounter = 200
+gameCounter++;
+console.log(gameCounter)
