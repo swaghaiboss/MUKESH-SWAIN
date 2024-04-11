2c0dcdc5d03ac82a3797321a8a93a9e0edf16103
swaghaiboss <156926330+swaghaiboss@users.noreply.github.com>
2024-04-11 05:09:01 +0000

    comparison

---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,18 @@
+// console.log(2 > 1)
+// console.log(3 >= 4)
+// console.log(2 < 3)
+// console.log(3 == 2)
+// console.log(2 != 4)
+
+// console.log("2" > 2)
+// console.log("02" > 1)
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null < 0)
+console.log(null >= 0)
+console.log(null <= 0)
+
+// === data type
+
+console.log("3" === 4)
